4a71b544eb509480214a36fc4f3ebdb6c40f3de6
behrooz2011 <bmmm1370@gmail.com>
2020-09-05 14:39:57 -0400

    js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var ss = document.getElementById("ss");
+ss.innerHTML="Bob"
+
+
+var a1 = document.getElementById("a1");
+a1.innerHTML="Persian Kebob"
+
+
+var a2 = document.getElementById("a2");
+a2.innerHTML=200
+
+var a3 = document.getElementsByClassName("a3");
+a3[0].innerHTML=130 + ' $'
+
+
+// var a1 = document.getElementById("ss");
+// a1.innerHTML="Bob"
